32edc979cbf3f37deee0107a6aaed49e8857cfe4
root <root@dal.emercoin.com>
2018-07-23 13:16:27 -0500

    Minor Update (v.1.8.7.4) Loads the recent transactions much faster.

--- html/ajax/get_recenttx.php
+++ html/ajax/get_recenttx.php
@@ -1,4 +1,7 @@
 	<?php
+		ini_set('display_startup_errors', 1);
+ini_set('display_errors', 1);
+error_reporting(-1);
 if (!empty($_COOKIE["lang"])) {
 	$lang=$_COOKIE["lang"];
 	require("../lang/".$lang.".php");
@@ -36,40 +39,62 @@ if (!empty($_COOKIE["lang"])) {
 	require_once __DIR__ . '/../../tools/include.php';
 }
 
+$txSkipValue = 1;
+$block_numtx = 0;
+
+$emc_info=$emercoin->getinfo();
+$block_height=$emc_info['blocks'];
+
+
 	echo '<div class="panel-heading"><b>'.lang('RECENT_TRANSACTIONS').'</b></div>
 	<table class="table">
 	<thead>
 	<tr><th>'.lang('TX_ID').'</th><th>'.lang('VALUE_EMC').'</th><th></th></tr>
 	</thead>
 	<tbody>';
-	$showBlocksQuery = "SELECT
-			t.txid,
-			t.valueout,
-			(c.id-t.blockid)+1 AS confirmations
-		FROM transactions t
-		JOIN (SELECT DISTINCT blockid
-				FROM transactions
-				WHERE fee > 0
-				ORDER BY blockid DESC
-				LIMIT 9) AS b
-		ON b.blockid = t.blockid AND t.fee > 0
-		CROSS JOIN (SELECT id
-				FROM blocks
-				ORDER BY id DESC
-				LIMIT 1) AS c
-		WHERE (c.id-t.blockid)+1 <= 9
-		ORDER BY t.blockid DESC";
-	$result = $dbconn->query($showBlocksQuery);
-	while($row = $result->fetch_assoc())
-	{
-		$txid=$row['txid'];
-		$tx_id_short = substr($txid, 0, 4)."...".substr($txid, -4);
-		$confirmations=$row['confirmations'];
-		if ($confirmations<3) {$labelcolor="danger";};
-		if ($confirmations>=3 && $confirmations<6) {$labelcolor="warning";};
-		if ($confirmations>=6) {$labelcolor="success";};
-		echo '<tr><td><a href="/tx/'.$txid.'" class="btn btn-primary btn-xs" role="button">'.$tx_id_short.'</a></td><td>'.$row["valueout"].'</td><td><span class="label label-'.$labelcolor.'">'.$confirmations.'</span></td></tr>';
-	}
+	for ($conf = 0;  $conf < 9; $conf++) {
+		echo $conf;
+		$block_hash=$emercoin->getblockhash($block_height-$conf);
+		$block=$emercoin->getblock($block_hash);
+		$flags=$block['flags'];
+		if ($flags == "proof-of-stake") {
+			$txSkipValue = 2;
+		} else {
+			$txSkipValue = 1;
+		}
+		$txs=$block['tx'];
+		$block_numtx = 0;
+		foreach ($txs as $tx) {
+			$tx_full=$emercoin->getrawtransaction($tx,1);
+			$block_numtx++;
+			$vout = 0;
+			$confirmations = $tx_full['confirmations'];
+			foreach ($tx_full['vin'] as $vin) {
+				if (isset($vin['txid'])){
+					if ($block_numtx > $txSkipValue) {
+						$vout += getTX_vout_value($emercoin, $vin['txid'], $vin['vout']);
+					}
+				}
+			}	
+			if ($block_numtx > $txSkipValue) {
+				$tx_id_short = substr($tx, 0, 4)."...".substr($tx, -4);
+				if ($confirmations<3) {$labelcolor="danger";};
+				if ($confirmations>=3 && $confirmations<6) {$labelcolor="warning";};
+				if ($confirmations>=6) {$labelcolor="success";};
+				echo '<tr><td><a href="/tx/'.$tx.'" class="btn btn-primary btn-xs" role="button">'.$tx_id_short.'</a></td><td>'.$vout.'</td><td><span class="label label-'.$labelcolor.'">'.$confirmations.'</span></td></tr>';
+			}
+		}
+	}	
 	echo "</tbody></table>";
 
+	
+function getTX_vout_value($emercoin, $txHash, $n) {
+	$tx=$emercoin->getrawtransaction($txHash,1);
+	$tx_vout=$tx['vout'];
+	foreach ($tx_vout as $vout) {
+		if ($vout['n'] == $n) {
+			return $vout['value'];
+		}
+	}
+}
 	?>
