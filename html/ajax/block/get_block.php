b1285f086d486b4445245dc8090563b51d6463f8
root <root@dal.emercoin.com>
2018-07-30 07:33:58 -0500

    Minor Update (v.1.9.3-U1)

--- html/ajax/block/get_block.php
+++ html/ajax/block/get_block.php
@@ -1,15 +1,9 @@
 	<?php
 	error_reporting(E_ALL);
 	ini_set("display_errors", 1);
-if (!empty($_COOKIE["network"])) {
-	$network=$_COOKIE["network"];
-	if ($network=='Mainnet') {
-		require_once __DIR__ . '/../../../tools/include.php';
-	} else if ($network=='Testnet') {
-		require_once __DIR__ . '/../../../tools/tinclude.php';
-	}
+if (explode('.', $_SERVER['HTTP_HOST'])[0] == "testnet") {
+	require_once __DIR__ . '/../../../tools/tinclude.php';
 } else {
-	setcookie("network","Mainnet",time()+(3600*24*14), "/");
 	require_once __DIR__ . '/../../../tools/include.php';
 }
 if (!empty($_COOKIE["lang"])) {
