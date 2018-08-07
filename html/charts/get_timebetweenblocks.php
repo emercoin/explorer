d92eff3187e2b48b898e4dc3c2e1fa505c2b0186
root <root@dal.emercoin.com>
2018-08-07 07:25:02 -0500

    Minor Chart update (v.1.9.4) New method to calculate the time between each block for the current day.

--- html/charts/get_timebetweenblocks.php
+++ html/charts/get_timebetweenblocks.php
@@ -7,7 +7,7 @@ if (explode('.', $_SERVER['HTTP_HOST'])[0] == "testnet") {
 }
 $query="SELECT (
 YEAR( FROM_UNIXTIME( `time` ) ) *3650 + MONTH( FROM_UNIXTIME( `time` ) ) *120 + DAY( FROM_UNIXTIME( `time` ) )
-) AS `day` , FROM_UNIXTIME( time ) AS time, (1400/COUNT( hash )) AS blocks
+) AS `day` , FROM_UNIXTIME( time ) AS time, (1440/COUNT( hash )) AS blocks, COUNT( hash ) AS countblocks
 FROM `blocks`
 WHERE id > '1'
 GROUP BY (
@@ -25,8 +25,12 @@ while($row = $result->fetch_assoc())
 	$time_epoch=strtotime($timeString);
 	$time_epoch =($time_epoch * 1000);
 	$day_array = array($time_epoch, round($row['blocks'],2));
+	$blocks = 1440/($row['countblocks']*bcdiv(86400,bcsub(time(),strtotime("today"),0),4));
 	array_push($days_array, $day_array);
 }
+array_pop($days_array);
+$day_array = array($time_epoch, round($blocks,2));
+array_push($days_array, $day_array);
 print json_encode($days_array, JSON_NUMERIC_CHECK);
 echo ");";
 ?>
